83471df9b575fee40aab47dbf9a78048a3e58250
Joshua Podhola <joshua.podhola@smoothstack.com>
2021-04-28 16:01:45 -0400

    Added Equals and HashCode

--- src/main/java/com/smoothstack/utopiaairlines/entities/TicketPK.java
+++ src/main/java/com/smoothstack/utopiaairlines/entities/TicketPK.java
@@ -8,4 +8,33 @@ public class TicketPK implements Serializable {
     private static final long serialVersionUID = 5464384774151807355L;
     private Traveller traveller;
     private Flight flight;
+
+    public Traveller getTraveller() {
+        return traveller;
+    }
+
+    public void setTraveller(Traveller traveller) {
+        this.traveller = traveller;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TicketPK ticketPK = (TicketPK) o;
+        return Objects.equals(traveller, ticketPK.traveller) && Objects.equals(flight, ticketPK.flight);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(traveller, flight);
+    }
 }
