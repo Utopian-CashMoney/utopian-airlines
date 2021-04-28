1767fdaf62959b90b76137265fd9809a4506305d
cpatelss90 <charvin.patel@smoothstack.com>
2021-04-28 00:25:31 -0500

    checking

--- src/main/java/com/smoothstack/utopiaairlines/ServletInitializer.java
+++ src/main/java/com/smoothstack/utopiaairlines/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.smoothstack.utopiaairlines;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//dd
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
